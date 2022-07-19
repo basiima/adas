3c0a98834e24027d61f13de98d2d5910985f69f3
Basiima Julius <mrbasiima@gmail.com>
2022-07-19 15:22:59 +0300

    add server.js

---
+++ backend/server.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const cors = require("cors");
+const path = __dirname + '/app/views/';
+const app = express();
+
+app.use(express.static(path));
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// database
+const db = require("./app/models");
+const Role = db.role;
+
+// db.sequelize.sync();
+// force: true will drop the table if it already exists
+db.sequelize.sync({force: true}).then(() => {
+  console.log('Drop and Resync Database with { force: true }');
+  initial();
+});
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to ADAS" });
+});
+
+// routes
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+
+function initial() {
+  Role.create({
+    id: 1,
+    name: "student"
+  });
+ 
+  Role.create({
+    id: 2,
+    name: "issuer"
+  });
+ 
+  Role.create({
+    id: 3,
+    name: "company"
+  });
+}
