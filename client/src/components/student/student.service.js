4b8306f71fb8894cd627a99026c0ac3a01f0b8df
iandrew8 <ibandaandrew8@gmail.com>
2022-07-20 19:00:34 +0300

    student backend logic

---
+++ client/src/components/student/student.service.js
@@ -0,0 +1,33 @@
+import http from "../../http-common";
+
+class StudentService {
+    static getAll() {
+      return http.get("/students");
+    }
+    
+    static get(id) {
+      return http.get(`/students/${id}`);
+    }
+
+    static create(data) {
+      return http.post("/students", data);
+    }
+
+    static update(id, data) {
+      return http.put(`/students/${id}`, data);
+    }
+
+    static delete(id) {
+      return http.delete(`/students/${id}`);
+    }
+
+    static deleteAll() {
+      return http.delete(`/students`);
+    }
+
+    static findByName(student_name) {
+      return http.get(`/students?title=${student_name}`);
+    }
+    
+  }
+export default StudentService;
