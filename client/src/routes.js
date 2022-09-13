4e1968184271c6299b3f79d19ee061278284119f
basiima <mrbasiima@gmail.com>
2022-09-13 17:09:54 +0300

    refactor Router to use individual route components

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,51 +1,21 @@
-import { Navigate, useRoutes } from 'react-router-dom';
-// layouts
-import DashboardLayout from './layouts/dashboard';
-import LogoOnlyLayout from './layouts/LogoOnlyLayout';
-//
-import Blog from './pages/Blog';
-import User from './pages/Students';
-import Login from './pages/Login';
-import NotFound from './pages/Page404';
-import Companies from './pages/Companies';
-import Requests from './pages/Requests';
-import DashboardApp from './pages/DashboardApp';
-import CertifyDocument from './pages/CertifyDocument';
-import AddRequest from './components/requests/AddRequest';
-import MakePayment from './components/payments/MakePayment';
+import React from 'react';
+import AuthService from './services/auth.service';
+import GeneralRouter from './routes/GeneralRoutes';
+import AdminRouter from './routes/AdminRoutes';
+import StudentRouter from './routes/StudentRoutes';
+import CompanyRouter from './routes/CompanyRoutes';
 
-// ----------------------------------------------------------------------
+const loggedInUser = AuthService.getCurrentUser();
+const loggedInUserRole = loggedInUser.roles;
 
 export default function Router() {
-  return useRoutes([
-    {
-      path: '/dashboard',
-      element: <DashboardLayout />,
-      children: [
-        { path: 'app', element: <DashboardApp /> },
-        { path: 'user', element: <User /> },
-        { path: 'companies', element: <Companies /> },
-        { path: 'documents', element: <Blog /> },
-        { path: 'requests', element: <Requests /> },
-        { path: 'certifyDocument', element: <CertifyDocument/> },
-        { path: 'place-request', element: <AddRequest/> }
-        
-      ],
-    },
-    {
-      path: '/',
-      element: <LogoOnlyLayout />,
-      children: [
-        { path: '/', element: <Navigate to="/login" /> },
-        { path: 'login', element: <Login /> },
-        { path: '404', element: <NotFound /> },
-        { path: '*', element: <Navigate to="/404" /> },
-        {path: '/payment', element: <MakePayment/>}
-       
-        
-      
-      ],
-    },
-    { path: '*', element: <Navigate to="/404" replace /> },
-  ]);
+//console.log(loggedInUserRole);
+  return (
+    <>
+      {loggedInUserRole == 'ROLE_ISSUER' && <AdminRouter /> }
+      {loggedInUserRole == 'ROLE_STUDENT' && <StudentRouter /> }
+      {loggedInUserRole == 'ROLE_COMPANY' && <CompanyRouter /> }
+      {loggedInUserRole == null && <GeneralRouter /> }
+    </>
+  );
 }
