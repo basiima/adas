81f11e718d1a9678b3e0fb7e546e15b047ab9d54
iandrew8 <ibandaandrew8@gmail.com>
2023-07-21 21:02:34 +0300

    modifications

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
 // scroll bar
-import 'simplebar/src/simplebar.css';
+//import 'simplebar/src/simplebar.css';
 
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
